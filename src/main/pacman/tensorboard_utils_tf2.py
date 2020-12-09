4dc178218428f508c4a383b324cfc8c92ed82ed8
thejose5 <thejusjose@outlook.com>
2020-12-09 01:45:22 -0500

    TSCL-Pacman test complete

--- src/main/pacman/tensorboard_utils_tf2.py
+++ src/main/pacman/tensorboard_utils_tf2.py
@@ -2,19 +2,21 @@ import tensorflow as tf
 
 
 def create_summary_writer(logdir):
-    return tf.summary.FileWriter(logdir)
+    return tf.summary.create_file_writer(logdir)
 
 
-
-def create_summary(tag, value):
-    return tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value)])
+def create_summary(tag, value, step):
+    return tf.summary.scalar(tag, value, step=step)
 
 
 def add_summary(writer, tag, value, step):
-    writer.add_summary(create_summary(tag, value), global_step=step)
+    with writer.as_default():
+        create_summary(tag, value, step)
+        writer.flush()
 
 
 def add_summaries(writer, tags, values, step, prefix=''):
     for (t, v) in zip(tags, values):
-        s = create_summary(prefix + t, v)
-        writer.add_summary(s, global_step=step)
+        with writer.as_default():
+            create_summary(prefix + t, v, step)
+            writer.flush()
